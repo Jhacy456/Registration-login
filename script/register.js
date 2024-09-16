49da986bdfc1ce0efd6e509fac8ca7680c7b3bcc
Jhacy456 <jakpesad20@gmail.com>
2024-09-16 11:44:51 +0000

    first commit

---
+++ script/register.js
@@ -0,0 +1,10 @@
+// select form by name
+const registerform = document.getElementById('register-form');
+// Handle form submit  event
+registerform.onsubmit = function() {
+    // Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send confirmation email
+}
