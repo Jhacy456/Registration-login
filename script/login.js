fbf595edbc4166e1ec994b491c19bcdb8d8b66bf
Jhacy456 <jakpesad20@gmail.com>
2024-09-18 11:53:15 +0000

    added new things

---
+++ script/login.js
@@ -0,0 +1,16 @@
+// select form by id
+const loginForm = document.querySelector('#login-form');
+console.log(loginForm);
+// Handle from submit event
+loginForm.addEventListener('submit', function(event){
+event.preventDefault();
+    // colect form data
+    const formData = new FormData(event.target);
+    console.log(
+        formData.get('username'), 
+        formData.get('password')
+    );
+    // Display success message
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent = 'Account logged in successfuly'
+});
